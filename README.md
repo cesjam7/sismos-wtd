df47fdb6575a991d3362da029dd50bc9e423f439
Cesar Aquino <cesar7492@gmail.com>
2015-08-08 02:04:08 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sismos What to do
+
+Aplicación web para enseñar a las personas que acciones realizar antes, durante y después de un sismo. Así como también números telefónicos que le serán de ayuda.
---
+++ controller/main.js
@@ -0,0 +1,51 @@
+var app = angular.module("SismosWtd", ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+	.when('/home', {
+		templateUrl :  'views/links.html'
+	})
+	.when('/procedimientos/:etapa', {
+		templateUrl :  'views/procedimientos.html',
+		controller: 'Procedimientos'
+	})
+	.when('/telefonos', {
+		templateUrl :  'views/telefonos.html',
+		controller: 'Telefonos'
+	})
+	.when('/about', {
+		templateUrl :  'views/about.html'
+	})
+	.otherwise({
+		redirectTo: '/home'
+	})
+
+}])
+
+app.controller("Procedimientos", function ($scope, $http, $routeParams, filterFilter) {
+	$http.get('../json/sismos_wtd.json').success(function (data) {
+
+		var filter = filterFilter(data.sismos_wtd.acciones, {
+			name: $routeParams.etapa
+		});
+		$scope.procedimientos = filter[0].procedimientos;
+		$scope.titulo = filter[0].titulo;
+
+		if($routeParams.etapa=="antes"){
+			$scope.icono = "reply";
+		}else if($routeParams.etapa=="durante"){
+			$scope.icono = "warning";
+		}else if($routeParams.etapa=="despues"){
+			$scope.icono = "share";
+		}
+
+	});
+})
+
+app.controller("Telefonos", function ($scope, $http, $routeParams) {
+	$http.get('../json/sismos_wtd.json').success(function (data) {
+
+		$scope.telefonos = data.sismos_wtd.telefonos;
+
+	});
+});
